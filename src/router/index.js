ce348e1871559981b82d8089be6ec89ba1b4d544
Juan García Hernández <juan@juan>
2021-02-17 23:24:31 +0100

    [ADD] ficheros del menú

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Inicio from "../views/Inicio.vue";
-import AcercaDe from "../views/AcercaDe.vue";
-import Articulos from "../views/Articulos.vue";
-import Articulo from "../views/Articulo.vue";
+import AcercaDe from "../views/Clasificacion.vue";
+import Articulos from "../views/Jornadas.vue";
+import Articulo from "../views/Equipos.vue";
 
 const routes = [
   {
     path: '/',
     name: 'Inicio',
     component: Inicio
-  },
-  {
-    path: '/acercaDe',
-    name: 'AcercaDe',
-    component: AcercaDe
-  },
-  {
-    path: '/articulos',
-    name: 'Articulos',
-    component: Articulos
-  },
-  {
-    path: '/articulos/:id',
-    name: 'Articulo',
-    component: Articulo
   }
 ]
 
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <img
-      src="https://img.pngio.com/download-logo-laliga-spain-football-svg-eps-png-psd-ai-vector-el-la-liga-logo-png-1158_1600.png"
-      alt="La Liga"
-    />
+      src="https://img.pngio.com/download-logo-laliga-spain-football-svg-eps-png-psd-ai-vector-el-la-liga-logo-png-1158_1600.png" alt="La Liga"/>
+      
   </div>
 </template>
 
