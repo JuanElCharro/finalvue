f19c0642c86e26f557ff6239d3f2697fb0536480
Juan García Hernández <juanelcharro@protonmail.com>
2021-02-18 13:56:50 +0100

    [ADD] Clasificación I (Equipos)

--- src/views/Clasificacion.vue
+++ src/views/Clasificacion.vue
@@ -1,7 +1,49 @@
 <template>
-    <div>Clasificacion</div>
+  <table>
+    <colgroup>
+      <col span="2" />
+    </colgroup>
+    <tr>
+      <th>Puesto</th>
+      <th>Nombre</th>
+      <th>País</th>
+      <th>Puntos</th>
+    </tr>
+    <tr span v-for="(equipos, index) in listaEquipos" :key="index">
+      <td>{{index}}</td>
+      <td>{{equipos.name}}</td>
+      <td>{{equipos.country}}</td>
+      <td>{{equipos.points}}</td>
+    </tr>
+  </table>
 </template>
 
-<script></script>
+<script>
+import axios from "axios";
+export default {
+data: () =>({
+    listaEquipos: []
+}),
+  created() {
+    axios.get("http://localhost:3000/clubs").then((result) => {
+      this.listaEquipos = result.data;
+    });
+  },
+};
+</script>
 
-<style></style>
+<style>
+p {
+  font-size: 20px;
+}
+
+table {
+  text-align: center;
+  margin-left: 15%;
+}
+
+th,
+td {
+  padding-left: 1em;
+}
+</style>
