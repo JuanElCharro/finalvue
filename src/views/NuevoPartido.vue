eacb245a46bc3cb05b326c21be57fc97d224f7c2
Juan García Hernández <juan@juan>
2021-02-18 21:45:02 +0100

    [ADD] Nuevo Partido (Incompleto)

--- src/views/NuevoPartido.vue
+++ src/views/NuevoPartido.vue
@@ -1,5 +1,48 @@
 <template>
-    <div>Nuevo Partido</div>
+  <div id="encuadre">
+    <p>Nuevo Partido</p>
+
+    <select v-model="equipoUnoSeleccionado" name="team1">
+      <option v-for="(partidos, index) in listaPartidos" :key="index">
+        {{ partidos.team1 }}
+      </option>
+    </select>
+
+    <select v-model="equipoDosSeleccionado" name="team2">
+      <option v-for="(partidos, index) in listaPartidos" :key="index">
+        {{ partidos.team2 }}
+      </option></select
+    ><br />
+
+    <input
+      v-model="golesEquipoUno"
+      type="number"
+      id="golesA"
+      name="golesA"
+      min="0"
+      max="100"
+    />
+
+    <input
+      v-model="golesEquipoDos"
+      type="number"
+      id="golesB"
+      name="golesB"
+      min="0"
+      max="100"
+    />
+    <br />
+
+    <input
+      v-model="fechaPartido"
+      type="date"
+      id="fechaPartido"
+      name="fechaPartido"
+    />
+    <br />
+
+    <input id="botonEnviar" type="submit" value="Enviar" />
+  </div>
 </template>
 
 <script>
@@ -16,4 +59,62 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+select {
+  background-color: DodgerBlue;
+  border: 2px solid DodgerBlue;
+  color: white;
+  font-size: 15px;
+  height: 35px;
+  padding: 5px;
+  width: 250px;
+  margin: 1em;
+}
+
+input {
+  margin: 1em;
+}
+
+#golesA {
+  width: 3em;
+  font-size: 20px;
+  border-radius: 10px;
+  border: 2px solid crimson;
+}
+
+#golesB {
+  width: 3em;
+  font-size: 20px;
+  border-radius: 10px;
+  border: 2px solid crimson;
+}
+
+#fechaPartido {
+  font-size: 20px;
+  border: 2px solid DodgerBlue;
+  border-radius: 12px;
+}
+
+#encuadre {
+  background-color: rgb(221, 221, 221);
+  margin-left: 20%;
+  margin-right: 20%;
+  border-radius: 30px;
+}
+
+p {
+  padding-top: 2em;
+}
+
+#botonEnviar {
+  margin: 2em;
+  background: #0066a2;
+  color: white;
+  border-style: outset;
+  border-color: #0066a2;
+  height: 50px;
+  width: 100px;
+  font: bold 15px arial, sans-serif;
+  text-shadow: none;
+}
+</style>
