5438b5da0058c8b2fdbff41ccd1c0dd17a65d7b6
Juan García Hernández <juanelcharro@protonmail.com>
2021-02-19 08:34:47 +0100

    [ADD] Vista Eliminar Jugador (Incompleta)

--- src/views/EliminarJugador.vue
+++ src/views/EliminarJugador.vue
@@ -1,7 +1,105 @@
 <template>
-    <div>Eliminar Jugador</div>
+  <div id="encuadre">
+    <div id="eliminarJugadorTitulo">ELIMINAR JUGADOR</div>
+
+    <select v-model="equipoSeleccionado" name="team1">
+      <option v-for="(equipos, index) in listaEquipos" :key="index">
+        {{ equipos.name }}
+      </option>
+    </select>
+
+    <select v-model="jugadorSeleccionado" name="team2">
+      <option v-for="(jugadores, index) in listaJugadores" :key="index">
+        {{ jugadores.name }}
+      </option></select
+    ><br />
+
+    <input id="botonEnviar" type="submit" value="Enviar" />
+  </div>
 </template>
 
-<script></script>
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    listaEquipos: [],
+    listaJugadores: [],
+  }),
+  created() {
+    axios.get("http://localhost:3000/clubs").then((result) => {
+      this.listaEquipos = result.data;
+    }),
+      axios.get("http://localhost:3000/players").then((result) => {
+        this.listaJugadores = result.data;
+      });
+  },
+};
+</script>
 
-<style></style>
+<style scoped>
+select {
+  background-color: DodgerBlue;
+  border: 2px solid DodgerBlue;
+  color: white;
+  font-size: 15px;
+  height: 35px;
+  padding: 5px;
+  width: 250px;
+  margin: 1em;
+}
+
+input {
+  margin: 1em;
+}
+
+#golesA {
+  width: 3em;
+  font-size: 20px;
+  border-radius: 10px;
+  border: 2px solid crimson;
+}
+
+#golesB {
+  width: 3em;
+  font-size: 20px;
+  border-radius: 10px;
+  border: 2px solid crimson;
+}
+
+#fechaPartido {
+  font-size: 20px;
+  border: 2px solid DodgerBlue;
+  border-radius: 12px;
+}
+
+#encuadre {
+  background-color: rgb(221, 221, 221);
+  margin-left: 20%;
+  margin-right: 20%;
+  border-radius: 30px;
+}
+
+p {
+  padding-top: 2em;
+}
+
+#botonEnviar {
+  margin: 2em;
+  background: #0066a2;
+  color: white;
+  border-style: outset;
+  border-color: #0066a2;
+  height: 50px;
+  width: 100px;
+  font: bold 15px arial, sans-serif;
+  text-shadow: none;
+}
+
+#eliminarJugadorTitulo {
+  font-size: 20px;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  color: #5e5757;
+}
+
+</style>
