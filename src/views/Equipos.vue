c969c86f6369f25529d9e93fdc97f11a02ba3041
Juan García Hernández <juanelcharro@protonmail.com>
2021-02-23 12:22:47 +0100

    [ADD] Nuevo Jugador en equipos. Práctica Finalizada

--- src/views/Equipos.vue
+++ src/views/Equipos.vue
@@ -1,5 +1,6 @@
 <template>
-    <table>
+<div id="floatTabla">
+  <table>
       <colgroup>
         <col span="2" />
       </colgroup>
@@ -7,19 +8,70 @@
         <th>{{ equipos.name }}</th>
         <td v-for="(jugadores, index) in listaJugadores" :key="index">
           <span v-if="equipos.name == jugadores.team">{{ jugadores.name }} 
-            <button @click="nuevoJugador()">Nuevo Jugador</button></span>
+            <button @click="mostrarBoton(), pasarNombreEquipo(equipos.name)">Nuevo Jugador</button></span>
         </td>
       </tr>
     </table>
+</div>
+    
+<div id="floatNuevoJugador" v-if="!isHidden">
+    <div span v-for="(jugadores, index) in listaJugadores" :key="index">
+      <span
+        v-if="jugadores.id == listaJugadores.length"
+        v-on:load="guardarId(jugadores.id)"
+      ></span>
+    </div>
+
+    <label for="nombre">Nombre Jugador:</label>
+    <input
+      v-model="nombreJugador"
+      type="text"
+      id="nombre"
+      name="nombre"
+      min="0"
+      max="100"
+    />
+    <br />
+
+    <label for="equipo">Equipo Jugador:</label>
+    <input
+      v-model="equipoJugador"
+      type="text"
+      id="equipo"
+      name="equipo"
+      min="0"
+      max="100"
+    />
+    <br />
+
+    <label for="goles">Goles Jugador:</label>
+    <input
+      v-model="golesJugador"
+      type="number"
+      id="goles"
+      name="goles"
+      min="0"
+      max="100"
+    />
+    <br />
+    
+    <button @click="guardarJugador()" id="botonEnviar">Enviar</button>
+</div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
   data: () => ({
+    isHidden: true,
     listaEquipos: [],
     listaJugadores: [],
     equipox: 0,
+
+    id: 0,
+    nombreJugador: "",
+    equipoJugador: "",
+    golesJugador: 0,
   }),
   async created() {
     await axios.get("http://localhost:3000/clubs").then((result) => {
@@ -30,9 +82,24 @@ export default {
       });
   },
   methods: {
-    nuevoJugador(){
-      window.location.href = 'http://localhost:8080/nuevojugador';
-    }
+    pasarNombreEquipo(nombreEquipo){
+      this.equipoJugador = nombreEquipo;
+    },
+    guardarJugador() {
+      axios.post("http://localhost:3000/players", {
+        id: parseInt(this.id, 10),
+        name: this.nombreJugador,
+        team: this.equipoJugador,
+        scores: parseInt(this.golesJugador, 10),
+      });
+      alert("Jugador Introducido Correctamente");
+    },
+    guardarId(id) {
+      this.id = id+1;
+    },
+    mostrarBoton() {
+      this.isHidden = false;
+    },
   }
 };
 </script>
@@ -78,6 +145,20 @@ tr:nth-child(even) {
 #info {
   padding-top: 1em;
   float: left;
+}
+
+#floatTabla{
+  float: left;
+  width: 75%;
+}
+
+#floatNuevoJugador{
+  float: right;
+  width: 20%;
+  border: 2px solid rgb(217, 255, 0);
+  background-color: rgb(255, 236, 172);
+  border: 2px solid rgb(0, 0, 0);
+  padding: 1%;
 }
 
 </style>
